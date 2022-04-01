73e641ede12727479b5aa6d51c015cff82e6b1a7
j0nl1 <jonlitech@gmail.com>
2022-04-01 06:03:30 +0200

    add messages definition

---
+++ src/msg.rs
@@ -0,0 +1,31 @@
+use schemars::JsonSchema;
+use cosmwasm_std::Addr;
+use serde::{Deserialize, Serialize};
+
+use crate::models::Todo;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub index: u64
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    AddTodo { description: String },
+    ChangeStatus { id: u64, status: u8 },
+    Delete { id: u64 }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    GetList { addr: Addr },
+    GetTodo { id: u64, addr: Addr }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct TodosResponse {
+    pub todos: Vec<(u64, Todo)>
+}
