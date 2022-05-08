795a60e3f3892f0ba71ebd80e799e9312c440545
j0nl1 <jonlitech@gmail.com>
2022-05-08 13:39:10 +0200

    add migration message

--- cw-contract/src/msg.rs
+++ cw-contract/src/msg.rs
@@ -10,6 +10,9 @@ pub struct InstantiateMsg {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     AddTodo { description: String },
