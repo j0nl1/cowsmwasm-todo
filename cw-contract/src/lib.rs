433b1688901595cfbfebc7e3f83891588dcf3c46
j0nl1 <jonlitech@gmail.com>
2022-05-08 13:21:07 +0200

    move integration tests to test folder

--- cw-contract/src/lib.rs
+++ cw-contract/src/lib.rs
@@ -1,13 +1,10 @@
-mod error;
-mod integration_tests;
 mod tests;
 
 pub mod contract;
+pub mod error;
 pub mod executions;
 pub mod helpers;
 pub mod models;
 pub mod msg;
 pub mod queries;
 pub mod state;
-
-pub use crate::error::ContractError;
